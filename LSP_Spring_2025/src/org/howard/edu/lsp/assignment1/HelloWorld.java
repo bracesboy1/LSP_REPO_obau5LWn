791e8e44362f9bf7f79a26f1e7dd2bbb3bf081db
brace <brace@laptop>
2025-01-23 12:58:08 -0500

    First Commit

--- LSP_Spring_2025/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ LSP_Spring_2025/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,9 +1,11 @@
 package org.howard.edu.lsp.assignment1;
 
 public class HelloWorld {
-
+	/**
+	* Name: Joshua Farmer
+	*/
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello World!!");
 
 	}
 
