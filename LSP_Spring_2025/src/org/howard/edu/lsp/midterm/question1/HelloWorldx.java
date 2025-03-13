371d935c98912142c08f36e28ca01908f5fc8385
brace <brace@laptop>
2025-03-13 19:25:38 -0400

    working

---
+++ LSP_Spring_2025/src/org/howard/edu/lsp/midterm/question1/HelloWorldx.java
@@ -0,0 +1,12 @@
+package org.howard.edu.lsp.midterm.question1;
+
+public class HelloWorldx {
+	/**
+	* Name: Joshua Farmer
+	*/
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+
+	}
+
+}
