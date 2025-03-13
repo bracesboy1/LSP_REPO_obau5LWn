a497ea410b429efa6f7a64a7393eeefa31a80a27
brace <brace@laptop>
2025-03-13 17:20:28 -0400

    working

---
+++ LSP_Spring_2025/src/org/howard/edu/lsp/midterm/HelloWorld.java
@@ -0,0 +1,12 @@
+package org.howard.edu.lsp.midterm;
+
+public class HelloWorld {
+	/**
+	* Name: Joshua Farmer
+	*/
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+
+	}
+
+}
