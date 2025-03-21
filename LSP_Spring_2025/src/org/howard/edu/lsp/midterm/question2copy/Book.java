8145fcd407dba7f4e2b80cedd549dd460a5afef3
Joshua Farmer <126531789+bracesboy1@users.noreply.github.com>
2025-03-20 20:01:45 -0400

    Add files via upload

---
+++ LSP_Spring_2025/src/org/howard/edu/lsp/midterm/question2copy/Book.java
@@ -0,0 +1,70 @@
+package org.howard.edu.lsp.midterm.question2copy;
+
+/**
+ * Represents a book in a library with title, author, ISBN, and year published.
+ */
+
+public class Book {
+    private String title;
+    private String author;
+    private String ISBN;
+    private int yearPublished;
+
+    // Constructor
+    public Book(String title, String author, String ISBN, int yearPublished) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.yearPublished = yearPublished;
+    }
+
+    // Getters
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    // Setters
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    // Override equals method to compare books based on ISBN and author
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Book book = (Book) obj;
+        return this.ISBN.equals(book.ISBN) && this.author.equals(book.author);
+    }
+
+    // Override toString method for string representation
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Author: " + author + ", ISBN: " + ISBN + ", Year Published: " + yearPublished;
+    }
+}
+
